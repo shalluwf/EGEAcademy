34face259b3e1748141f7e8b563ba655433f6f29
toropygawf <shallumeister02@gmail.com>
2026-05-02 18:37:56 +0300

    Predeploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
+  base: '/EGEAcademy',
   css: {
     postcss: {
       plugins: [autoprefixer()],
